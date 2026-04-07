b93372a6ad9a80fe9080ec144692ab42e466f820
Hussein Shiri <hussein.y.shiri18@gmail.com>
2026-04-07 16:14:16 +0300

    added streamlit first app

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("Hello World")
+import streamlit as st
+from supabase import create_client
+
+url = "https://hwxfcsyvvrogbhmtszih.supabase.co"
+key = "sb_secret_xv9oY0irqBVqF2Skpy4Szw_gKpBc-vs"
+supabase = create_client(url, key)
+
+st.title("Salary Prediction Dashboard")
+
+data = supabase.table("predictions").select("*").order("created_at", desc=True).execute().data
+
+if not data:
+    st.warning("No predictions yet.")
+else:
+    for row in data:
+        st.subheader("Prediction")
+
+        st.json(row["inputs"])
+        st.metric("Predicted Salary", f"${row['predicted_salary']:,.0f}")
+        st.write(row["narrative"])
+
+        if row["chart_url"]:
+            st.image(row["chart_url"])
